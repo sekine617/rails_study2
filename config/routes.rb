a1f18fa5b67d9849d8b6dfeb7321033dbf6bad1c
sekine617 <seweb14wm54@icloud.com>
2020-12-11 23:48:46 +0900

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/me'
+
+  get 'home/index'
+
+  get 'session/create'
+
+  get 'session/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   resources :boards
