05c495ac83bb4a4bcb3ce24dacddf71a68f5d125
sekine617 <seweb14wm54@icloud.com>
2020-12-12 23:05:28 +0900

    add login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,7 +1,15 @@
 class SessionController < ApplicationController
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.user_id
+      redirect_to mypage_path
+    else
+      render 'home/index'
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/me'
-
-  get 'home/index'
-
-  get 'session/create'
-
-  get 'session/destroy'
+  get 'mypage' => 'users#me'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#cdestroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  resources :users, only: %i[create new]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
